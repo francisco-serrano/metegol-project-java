1d55dc162fc399119f5848ef588fdd32d2cd033d
Francisco Serrano <francisco.serrano372@gmail.com>
2020-02-17 02:14:38 -0300

    fixes test environment

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user`
 (
     `id`   int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) DEFAULT NOT NULL,
+    `name` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
--- src/test/java/com/github/franciscoserrano/metegolproject/services/ReadyServiceTest.java
+++ src/test/java/com/github/franciscoserrano/metegolproject/services/ReadyServiceTest.java
@@ -3,6 +3,7 @@ package com.github.franciscoserrano.metegolproject.services;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
